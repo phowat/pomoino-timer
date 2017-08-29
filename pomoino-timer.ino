c35aadb381b185116886ed969407eda57e01b95c
Pedro Howat <pedro@instant.com.br>
2017-08-29 00:36:24 -0300

    Fixes timer, each second in the timer was not a real second before.

--- pomoino-timer.ino
+++ pomoino-timer.ino
@@ -8,7 +8,12 @@ int LeftBtnState = 0;
 int RightBtnState = 0;
 
 int ptime = 0;
+
+double a = millis();
+double c; 
+
 char ftime[6];
+int pomodoroCount = 10;
 
 const int pomodoroDuration = 25 * 60;
 const int shortBreak = 5 * 60;
@@ -18,21 +23,30 @@ const int longBreakInterval = 4;
 typedef enum {STOPPED, WORKING, BREAK} StateType;
 StateType curState = STOPPED;
 
+
+void printStatus() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Pomodoros: ");
+  lcd.print(pomodoroCount);
+  sprintf(ftime, "%02d:%02d", (ptime)/60, (ptime )%60);
+  lcd.setCursor(0,1);
+  lcd.print(ftime);
+}
+
 void setup() {
   lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print("Pomodoros: 0");
 
   
   pinMode(Buzzer, OUTPUT);
   pinMode(LeftBtn, INPUT);
   pinMode(RightBtn, INPUT);
-
+  printStatus();
 }
 
 void loop() {
 
-  delay(1000);
+  delay(100);
 
   LeftBtnState = digitalRead(LeftBtn);
   RightBtnState = digitalRead(RightBtn);
@@ -40,19 +54,23 @@ void loop() {
   if (curState == STOPPED) {
     if (LeftBtnState) {
       curState = WORKING;
+      pomodoroCount = 20;
+    }
+
+    if (RightBtnState) {
+      pomodoroCount = 0;
     }
   } else if (curState == WORKING) {
     if (RightBtnState) {
       curState = STOPPED;
+      a = millis();
       ptime = 0;
     } else {
-      ptime++;
+      c = millis();
+      ptime = (c - a) / 1000;
     }
   }
+  printStatus();
 
 
-  sprintf(ftime, "%02d:%02d", ptime/60, ptime%60);
-  lcd.setCursor(0,1);
-  lcd.print(ftime);
-//  noTone(Buzzer);
 }
